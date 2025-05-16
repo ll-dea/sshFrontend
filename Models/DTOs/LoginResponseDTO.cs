5020d297720948e33952e580c1509f04547a54b6
Aulona Ramosaj <aulona.ramosaj@student.uni-pr.edu>
2025-05-16 11:13:29 +0200

    Dtos from back

---
+++ Models/DTOs/LoginResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace sshBackend1.Models.DTOs
+{
+    public class LoginResponseDTO
+    {
+        public ApplicationUserDTO User { get; set; }
+        public string Token { get; set; }
+    }
+}
+
